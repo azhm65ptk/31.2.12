cc2a91eef2a4d92662c3dd54e435e6f8d5656dd3
Aung Myint <Austin@AungZinyintsMBP.lan>
2021-11-23 11:50:37 -0500

    solving with promises

--- NumFacts/app.js
+++ NumFacts/app.js
@@ -2,27 +2,21 @@ let favNumber = 5;
 let baseURL = "http://numbersapi.com";
 
 // 1.
-async function part1() {
-  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
   console.log(data);
-}
-part1();
+});
 
 // 2.
-const favNumbers = [7, 11, 22];
-async function part2() {
-  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
+let favNumbers = [7, 11, 22];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
   console.log(data);
-}
-part2();
+});
 
 // 3.
-async function part3() {
-  let facts = await Promise.all(
-    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
-  );
-  facts.forEach(data => {
-    $('body').append(`<p>${data.text}</p>`);
-  });
-}
-part3();
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
