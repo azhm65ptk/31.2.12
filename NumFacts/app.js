6c90e9f197fe8caa596176d25695c6620972d7e7
Aung Myint <Austin@AungZinyintsMBP.lan>
2021-11-23 11:35:09 -0500

    solving using async/awit"

--- NumFacts/app.js
+++ NumFacts/app.js
@@ -1,20 +1,28 @@
-let favNum=6;
-let baseURL="http://numbersapi.com";
+let favNumber = 5;
+let baseURL = "http://numbersapi.com";
 
+// 1.
+async function part1() {
+  let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+  console.log(data);
+}
+part1();
 
-//1
-$.getJSON(`${baseURL}/${favNum}?json`).then(res => console.log(res));
+// 2.
+const favNumbers = [7, 11, 22];
+async function part2() {
+  let data = await $.getJSON(`${baseURL}/${favNumbers}?json`);
+  console.log(data);
+}
+part2();
 
-
-
-let favNumbers=[6,15,24]
-$.getJSON(`${baseURL}/${favNumbers}/json`).then(data => console.log(data));
-
-
-Promise.all(
-    Array.from({length:4}, ()=>{
-        return $.getJSON(`${baseURL}/${favNum}?json`);
-    })
-).then(fact =>{
- fact.forEach(data=>$('body').append(`<p> ${data.text}</p>`));
-});
+// 3.
+async function part3() {
+  let facts = await Promise.all(
+    Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+  );
+  facts.forEach(data => {
+    $('body').append(`<p>${data.text}</p>`);
+  });
+}
+part3();
